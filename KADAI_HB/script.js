83f77ca758fda0c773afb83d782bdc2e1a05c5f3
Rin-stars <lilac.ja1208@gmail.com>
2022-01-07 21:06:02 -0800

    0107 2105 modified sidebar

--- KADAI_HB/script.js
+++ KADAI_HB/script.js
@@ -2,18 +2,18 @@ $(function(){
 
   $('.l-header__menu').click(function() {
     $('.p-sidebar').toggleClass('is-open');
-    $(".js-header__cover").css( 'height' , '100%' );
+    $(".l-header__cover").css( 'height' , '100%' );
   });
 
   $('.p-sidebar__btn').click(function() {
     $('.p-sidebar').toggleClass('is-open');
-    $(".js-header__cover").css( 'height' , '0' );
+    $(".l-header__cover").css( 'height' , '0' );
   });
 
   // ウィンドウ幅を変更時の処理。サイドバーを閉じる。
   $(window).resize(function(event){
     $('.p-sidebar').removeClass('is-open'); // is-openクラス撤去。
-    $(".js-header__cover").css( 'height' , '0' ); // 灰色背景の撤去。
+    $(".l-header__cover").css( 'height' , '0' ); // 灰色背景の撤去。
   });
 
 });
