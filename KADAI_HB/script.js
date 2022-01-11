10f6c7a9be6fdff87dbad1af6c3eed6afc370f86
Rin-stars <lilac.ja1208@gmail.com>
2022-01-10 22:50:31 -0800

    organize scssfiles

--- KADAI_HB/script.js
+++ KADAI_HB/script.js
@@ -1,19 +1,19 @@
 $(function(){ 
 
-  $('.l-header__menu').click(function() {
+  $('.c-button--open').click(function() {
     $('.p-sidebar').toggleClass('is-open');
-    $(".l-header__cover").css( 'height' , '100%' );
+    $('.c-body__cover').css( 'height' , '100%' );
   });
 
-  $('.p-sidebar__btn').click(function() {
+  $('.c-button--close').click(function() {
     $('.p-sidebar').toggleClass('is-open');
-    $(".l-header__cover").css( 'height' , '0' );
+    $('.c-body__cover').css( 'height' , '0' );
   });
 
   // ウィンドウ幅を変更時の処理。サイドバーを閉じる。
   $(window).resize(function(event){
     $('.p-sidebar').removeClass('is-open'); // is-openクラス撤去。
-    $(".l-header__cover").css( 'height' , '0' ); // 灰色背景の撤去。
+    $('.c-body__cover').css( 'height' , '0' ); // 灰色背景の撤去。
   });
 
 });
