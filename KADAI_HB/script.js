1d59d6c851e26654d8c47ea141383283b2303d30
Rin-stars <lilac.ja1208@gmail.com>
2022-01-19 22:56:08 -0800

    organized scssfile

--- KADAI_HB/script.js
+++ KADAI_HB/script.js
@@ -2,20 +2,20 @@ $(function(){
 
   // ヘッダーのMenuボタンクリック時にサイドバーを開く。
   $('.c-nav__btn--open').click(function() {
-    $('.p-sidebar').toggleClass('is-open');
-    $('.c-cover').css( 'height' , '100%' );
+    $('.l-sidebar').toggleClass('is-open');
+    $('.p-nav__bg').css( 'height' , '100%' );
   });
 
   // サイドバーの×ボタンクリック時にサイドバーを閉じる。
   $('.c-nav__btn--close').click(function() {
-    $('.p-sidebar').toggleClass('is-open');
-    $('.c-cover').css( 'height' , '0' );
+    $('.l-sidebar').toggleClass('is-open');
+    $('.p-nav__bg').css( 'height' , '0' );
   });
 
   // ウィンドウ幅を変更時の処理。サイドバーを閉じる。
   $(window).resize(function(event){
-    $('.p-sidebar').removeClass('is-open'); // is-openクラス撤去。
-    $('.c-cover').css( 'height' , '0' ); // 灰色背景の撤去。
+    $('.l-sidebar').removeClass('is-open'); // is-openクラス撤去。
+    $('.p-nav__bg').css( 'height' , '0' ); // 灰色背景の撤去。
   });
 
 });
